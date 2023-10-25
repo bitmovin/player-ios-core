528e7cb488cf9a201dd7c8476e1c0ff8fa10277a
Bitmovin Release Automation <support@bitmovin.com>
2023-10-25 08:44:44 +0200

    Release 3.48.0-a.8

--- Package@swift-5.8.swift
+++ Package@swift-5.8.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BitmovinPlayerCore",
-            url: "https://cdn.bitmovin.com/player/ios_tvos/3.48.0-a.6/BitmovinPlayerCore.zip",
-            checksum: "7fbb2014e1a1e54bdb6ab2f17fe98a6101d510c0fc41701d7551924bb6814266"
+            url: "https://cdn.bitmovin.com/player/ios_tvos/3.48.0-a.8/BitmovinPlayerCore.zip",
+            checksum: "a2d32003c30c46b07e319e65dc40076305fcfdfe888a4902d2df87d8bbc3f864"
         )
     ]
 )
