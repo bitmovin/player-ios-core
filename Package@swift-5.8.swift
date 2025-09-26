21f2cc2a2e294133f01497241dd140281451691c
Bitmovin Release Automation <player-sdks@bitmovin.com>
2025-09-26 14:13:03 +0200

    Release 3.97.1

--- Package@swift-5.8.swift
+++ Package@swift-5.8.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BitmovinPlayerCore",
-            url: "https://cdn.bitmovin.com/player/ios_tvos/3.97.1-rc.1/BitmovinPlayerCore.zip",
-            checksum: "17e71cad0b8cb6bd45eb81c963a07a70ad0c9e39165f0f04b4826693eb580092"
+            url: "https://cdn.bitmovin.com/player/ios_tvos/3.97.1/BitmovinPlayerCore.zip",
+            checksum: "ab7016bf440605d7cc5b9b0694fa8fe986b854d1b32c0613dba9ede49296c744"
         )
     ]
 )
